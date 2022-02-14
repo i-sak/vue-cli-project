1f05b8cde38337f2fb982f06b62f45b59db6a110
isaac <isaac7263@naver.com>
2022-02-14 21:20:00 +0900

    mixin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div style="background-color : skyblue; padding : 5px;">
-    <h1> user.name : {{ user.name }} </h1>
-    <h1> user.age : {{ user.age }} </h1>
+    <h2> user.name : {{ user.name }} </h2>
+    <h2> user.age : {{ user.age }} </h2>
+    <h2> {{ getDateAndTime(createAt) }} </h2>
     <v-btn elevation="4" @click="changeName()">이름 변경</v-btn>
     <home-component 
       :name="user.name"
@@ -18,6 +19,7 @@
 
 <script>
 import homeComponent from "./Home.vue"  // .vue 는 생략 가능
+import { dateFormat } from "./mixins/dateFormat"
 
 // es6에서 모듈을 추출하는 방식 중 하나 export default
 export default {
@@ -29,8 +31,12 @@ export default {
     user : {
       name : "vuer",
       age : 20
-    }
+    },
+    createAt : null,
   }),
+  created () {
+    this.createAt = new Date()
+  },
   methods : {
     changeName () { // changeName = function() { }
       this.name = "Vuetify"
@@ -39,7 +45,8 @@ export default {
       console.log(user)
       this.user.name = user.name
       this.user.age = user.age
-    }
-  }
+    },
+  },
+  mixins : [dateFormat]
 }
 </script>
--- src/Status.vue
+++ src/Status.vue
@@ -3,22 +3,33 @@
         <h2> {{ title }} </h2>
         <h2> {{ user.name }}</h2>
         <h2> {{ user.age }}</h2>
+        <h2> {{ getDateAndTime(date)}} </h2>
     </div>
 </template>
 <script>
+import { dateFormat } from "./mixins/dateFormat"
+
 export default {
     data : () => ({
         title : "Good!",
         user : {
             name : 'status',
             age : 25
-        }
+        },
+        date : null
     }),
+    created() {
+        this.date = new Date()
+    },
     mounted() {
         this.emitter.on('sibling', user => {
             this.user.name = user.name
             this.user.age = user.age
         })
-    }
+    },
+    methods : {
+        
+    },
+    mixins : [dateFormat]
 }
 </script>
